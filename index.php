e97e9956f7515e57f79ab7289f10ab10347308a1
coduxcompany <eng.ahmedmohamed.2002@gmail.com>
2021-04-21 02:16:55 +0200

    kidsarea

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+    <title>Kids Area Dashboard | Sary Academy</title>
+    <?php include('links.php'); ?>
+</head>
+<body style="background-color:#FAFAFA">
+
+<div class="container" style="background-color: white;position: relative;top: 15%;min-height:600px">
+    <div class="row">
+        <div class="col-lg">
+            
+        </div>
+        <div class="col-lg" style="background: url('pics/wooden-playground-area.jpg');">
+        
+        </div>
+    </div>
+</div>
+
+</body>
+<?php include('scripts.php') ?>
+</html>
