ecfd1faaef337b8f4e8fdaaacafdae2dde251e3f
Ahmed Mohamed Ibrahim <eng.ahmedmohamed.2002@gmail.com>
2021-05-05 02:55:53 +0200

    Update api.php

--- api.php
+++ api.php
@@ -60,6 +60,8 @@ if(!empty($_POST['code']) && !empty($_POST['tag'])){
                 if(mysqli_num_rows($query_stop) > 0) {
                     $query_s = mysqli_query($connect, "UPDATE sessions SET status='Stopped' WHERE code = '$code' AND date='$date' AND status=''");
                     echo "Timer Stopped";
+                    $sql_activity_stopped = "INSERT INTO activities (code, date, time, tag) VALUES ('$code', '$date', '$time', 'Timer Stopped')";
+                    $query_activity_stopped = mysqli_query($connect, $sql_activity_stopped);        
 
                     } else {
                         $hour = $row_hour['hours'];
