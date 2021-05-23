cc3c40269a7de4e93437b5c9e2594eccb7ceba50
Ahmed Mohamed Ibrahim <eng.ahmedmohamed.2002@gmail.com>
2021-05-23 16:21:26 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,15 +1,15 @@
 <?php
-$host = "bmlx3df4ma7r1yh4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306";
-$user = "tfkx1mkuw3590scm";
-$pass = "amj1es3fhfnt4kw5";
-$db = "f6yog60g77dtmdrl";
+//$host = "bmlx3df4ma7r1yh4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306";
+//$user = "tfkx1mkuw3590scm";
+//$pass = "amj1es3fhfnt4kw5";
+//$db = "f6yog60g77dtmdrl";
 
-//$host = "localhost";
-//$user = "root";
-//$pass = "";
-//$db = "kidsarea";
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "kidsarea";
 $connect = mysqli_connect($host, $user, $pass, $db) or die("Can't connect with database");
-mysqli_set_charset($connect,"utf8");
+mysqli_set_charset($connect, "set names 'utf8'");
 
 session_start();
 
